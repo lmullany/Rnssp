f158466a6fa36dea371bf51db2222081da167903
Mullany, Luke C <luke.mullany@jhuapl.edu>
2026-02-11 10:46:12 -0500

    moved non-standard files to inst, fix the zzz.R on attach function

--- R/zzz.R
+++ R/zzz.R
@@ -14,21 +14,39 @@ Rnssp RMD Templates Documentation: https://cdcgov.github.io/Rnssp-rmd-templates
 
 Run 'Rnssp_vignettes()' to browse all Rnssp vignettes.
 ")
+  
+  # if we are running R CMD check, we shouldn't do this network call:
+  if(!interactive() || nzchar(Sys.getenv("_R_CHECK_PACKAGE_NAME_"))) {
+    return(invisible())
+  }
+  
+  # Otherwise, we can check, although this should really be a user option
+  
   url <- "https://raw.githubusercontent.com/cdcgov/Rnssp/master/DESCRIPTION"
-
-  x <- suppressWarnings(try(readLines(url), silent = TRUE))
-  remote_version <- gsub("Version:\\s*", "", x[grep('Version:', x)])
-  installed_version <- tryCatch(packageVersion(gsub(".*/", "", "Rnssp")), error=function(e) NA)
-
-  if (all(remote_version > installed_version, class(x) != "try-error")) {
+  
+  x <- try(readLines(url, warn = FALSE), silent = TRUE)
+  if (inherits(x, "try-error")) return(invisible())
+  
+  ver_line <- x[grep("^Version:", x)]
+  if (length(ver_line) == 0) return(invisible())
+  
+  remote_version_chr <- sub("^Version:\\s*", "", ver_line[1])
+  remote_version <- try(numeric_version(remote_version_chr), silent = TRUE)
+  if (inherits(remote_version, "try-error")) return(invisible())
+  
+  installed_version <- utils::packageVersion("Rnssp")
+  
+  if (installed_version < remote_version) {
     cli::cli_alert_info(
       paste0(
-        'Rnssp v',
-        installed_version,
-        ' is outdated!\n## Please upgrade to latest Rnssp v',
+        "Rnssp v", installed_version,
+        " is outdated!\n## Please upgrade to latest Rnssp v",
         remote_version,
-        ': {.code devtools::install_github("cdcgov/Rnssp")}')
+        ': {.code devtools::install_github("cdcgov/Rnssp")}'
+      )
     )
   }
+  
+  invisible()
 }
 
